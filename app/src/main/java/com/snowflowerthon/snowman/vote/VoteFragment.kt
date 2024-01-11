172e269316d8b985993ccc0c99c48cfa906b49a4
yujin <jini1514@soongsil.ac.kr>
2024-01-12 02:03:44 +0900

    feat: cloth에서는 로그 찍힘

--- app/src/main/java/com/snowflowerthon/snowman/vote/VoteFragment.kt
+++ app/src/main/java/com/snowflowerthon/snowman/vote/VoteFragment.kt
@@ -2,15 +2,23 @@ package com.snowflowerthon.snowman.vote
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.snowflowerthon.snowman.databinding.FragmentVoteBinding
+import com.snowflowerthon.snowman.vote.category.ClothesFragment
 import com.snowflowerthon.snowman.vote.category.TabPagerAdapter
 
-class VoteFragment : BottomSheetDialogFragment() {
+interface PenguinInteractionListener {
+    fun dressPenguinWithScarf()
+}
+
+class VoteFragment : Fragment(), PenguinInteractionListener {
 
 
     private var _binding: FragmentVoteBinding? = null
@@ -27,20 +35,46 @@ class VoteFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 탭 레이아웃의 탭 이름을 정의합니다. 여기서는 "Tab 1"과 "Tab 2"로 설정하였습니다.
-        val tabNames = listOf("Tab 1", "Tab 2")
+//        // 탭 레이아웃의 탭 이름을 정의합니다. 여기서는 "Tab 1"과 "Tab 2"로 설정하였습니다.
+        val aFragment = ClothesFragment()
+        aFragment.setInteractionListener(this) // VoteFragment의 인스턴스를 전달
 
+
+        val tabNames = listOf("아우터", "아이템")
         binding.myPageviewPager.adapter = TabPagerAdapter(this, tabNames.size)
 
         // 뷰페이저와 탭 레이아웃을 연결합니다.
         TabLayoutMediator(binding.myPagetabLayout, binding.myPageviewPager) { tab, position ->
             tab.text = tabNames[position]
         }.attach()
+
+//        // AFragment를 뷰페이저에 추가
+//        val aFragment = ClothesFragment()
+//        aFragment.setInteractionListener(this) // VoteFragment의 인스턴스를 전달
+//
+//        val viewPager2 = binding.myPageviewPager
+//        val adapter = TabPagerAdapter(this, 2) // 예시로 탭이 2개라 가정
+//        adapter.addFragment(aFragment)
+//        // 다른 프래그먼트 추가
+//
+//        viewPager2.adapter = adapter
+//
+//        // 뷰페이저와 탭 레이아웃 연결
+//        TabLayoutMediator(binding.tabLayout, viewPager2) { tab, position ->
+//            tab.text = "Tab ${position + 1}"
+//        }.attach()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // VoteFragment에게 펭귄에게 목도리를 씌우는 동작 수행
+    override fun dressPenguinWithScarf() {
+        // 원하는 동작을 여기에 추가하세요
+        Log.d("VoteFragment","롱패")
+        Toast.makeText(requireContext(), "펭귄에게 목도리를 씌웠습니다!", Toast.LENGTH_SHORT).show()
     }
 }
 
--- app/src/main/java/com/snowflowerthon/snowman/vote/category/ClothesFragment.kt
+++ app/src/main/java/com/snowflowerthon/snowman/vote/category/ClothesFragment.kt
@@ -1,60 +1,64 @@
 package com.snowflowerthon.snowman.vote.category
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.snowflowerthon.snowman.R
+import com.snowflowerthon.snowman.databinding.FragmentClothesBinding
+import com.snowflowerthon.snowman.databinding.FragmentVoteBinding
+import com.snowflowerthon.snowman.vote.PenguinInteractionListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ClothesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ClothesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private var _binding: FragmentClothesBinding? = null
+    private val binding get() = _binding!!
+
+    private var interactionListener: PenguinInteractionListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clothes, container, false)
+    ): View {
+        _binding = FragmentClothesBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ClothesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ClothesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is PenguinInteractionListener) {
+            interactionListener = context
+        } else {
+            throw RuntimeException("$context must implement PenguinInteractionListener")
+        }
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 목도리 아이템 클릭 이벤트 처리
+        binding.btnLongPadding.setOnClickListener {
+            // VoteFragment에 알리기
+            interactionListener?.dressPenguinWithScarf()
+            Log.d("AFragment", "ImageButton Clicked")
+
+        }
+    }
+
+
+
+    // VoteFragment에서 호출할 수 있도록 설정
+    fun setInteractionListener(listener: PenguinInteractionListener) {
+        interactionListener = listener
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
