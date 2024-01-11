96e7440f974cdfd78300d90f851f6f39b5a971bb
yujin <jini1514@soongsil.ac.kr>
2024-01-12 03:15:08 +0900

    add: enum

---
+++ app/src/main/java/com/snowflowerthon/snowman/data/enums/Clothes.kt
@@ -0,0 +1,9 @@
+package com.snowflowerthon.snowman.data.enums
+
+enum class Clothes{
+    NONE,
+    LONG_SLEEVE, NEAT,
+    LONG_PADDING, SHORT_PADDING, COAT,
+    SCARF,
+    EAR_MUFFS, BALACLAVA;
+}
