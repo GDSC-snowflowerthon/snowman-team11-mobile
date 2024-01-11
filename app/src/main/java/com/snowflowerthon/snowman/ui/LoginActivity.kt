4c9e2c001d000d65babe4f3b53deafc144112b61
Seoa-S <0325sandy@ewhain.net>
2024-01-12 04:26:55 +0900

    error solve

--- app/src/main/java/com/snowflowerthon/snowman/ui/LoginActivity.kt
+++ app/src/main/java/com/snowflowerthon/snowman/ui/LoginActivity.kt
@@ -2,13 +2,11 @@ package com.snowflowerthon.snowman.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD:app/src/main/java/com/snowflowerthon/snowman/LoginActivity.kt
+import com.snowflowerthon.snowman.R
 import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
-=======
-import com.snowflowerthon.snowman.R
->>>>>>> 42080928659506ee9c3b139ce7fe7460b99f8c1d:app/src/main/java/com/snowflowerthon/snowman/ui/LoginActivity.kt
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
