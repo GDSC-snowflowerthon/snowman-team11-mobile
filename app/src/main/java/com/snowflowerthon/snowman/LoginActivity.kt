34b9ff8b43563d002ae103b315c9382ba58a54ff
Seoa-S <0325sandy@ewhain.net>
2024-01-12 03:47:20 +0900

    add files

--- app/src/main/java/com/snowflowerthon/snowman/LoginActivity.kt
+++ app/src/main/java/com/snowflowerthon/snowman/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.snowflowerthon.snowman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        KakaoSdk.init(this, "69a3cfd1a816a40b18dc20cd4b8a300d")
         setContentView(R.layout.activity_login)
+
+
+
+
+
     }
 }
--- app/src/main/java/com/snowflowerthon/snowman/MainActivity.kt
+++ app/src/main/java/com/snowflowerthon/snowman/MainActivity.kt
@@ -2,12 +2,15 @@ package com.snowflowerthon.snowman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
         var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
