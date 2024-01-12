7dadc1bfea8f08f25f7c45f87bf200e654f84776
Seoa-S <0325sandy@ewhain.net>
2024-01-13 03:19:47 +0900

    feat: popup ã기능 구현

--- app/src/main/java/com/snowflowerthon/snowman/ui/archive/ArchiveFragment.kt
+++ app/src/main/java/com/snowflowerthon/snowman/ui/archive/ArchiveFragment.kt
@@ -1,5 +1,6 @@
 package com.snowflowerthon.snowman.ui.archive
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.snowflowerthon.snowman.PopupActivity
 import com.snowflowerthon.snowman.R
 import com.snowflowerthon.snowman.data.ApiService
 import com.snowflowerthon.snowman.data.RetrofitClient
@@ -39,7 +41,6 @@ class ArchiveFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = binding.rvArchive
-
 
         lodeData()
     }
@@ -74,6 +75,8 @@ class ArchiveFragment : Fragment() {
 //                    Log.d("VoteFragment", response.body()?.success.toString() + response.body()?.data.toString())
                 }
             })
-        }
+    }
+
+
 
 }
