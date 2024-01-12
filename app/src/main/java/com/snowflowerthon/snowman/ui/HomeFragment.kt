a4b0a85ba67511473a3dbc9efa7c96b47a57f00d
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2024-01-12 23:16:29 +0900

    feat: 위치, 시간 실시간 반영

--- app/src/main/java/com/snowflowerthon/snowman/ui/HomeFragment.kt
+++ app/src/main/java/com/snowflowerthon/snowman/ui/HomeFragment.kt
@@ -1,5 +1,9 @@
 package com.snowflowerthon.snowman.ui
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -7,10 +11,9 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.snowflowerthon.snowman.R
 import com.snowflowerthon.snowman.databinding.FragmentHomeBinding
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -48,16 +51,40 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
         val handler = Handler(Looper.getMainLooper())
 
-        //val location =
+        var locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val permissions = arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        // 위치 권한 허가
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                permissions,
+                1
+            )
+        }
+
+        var currentLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
         val dateTimeFormat = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm:ss")
         var now = LocalDateTime.now().format(dateTimeFormat)
 
-        binding.tvCurrentInfo.text = now
+        binding.tvCurrentInfo.text = "위도:${currentLocation?.latitude} 경도:${currentLocation?.longitude}\n$now"
 
         val updateDuringRuntime = object : Runnable {
             override fun run() {
                 // Update the time in the TextView
-                binding.tvCurrentInfo.text = LocalDateTime.now().format(dateTimeFormat)
+                val now = LocalDateTime.now().format(dateTimeFormat)
+                binding.tvCurrentInfo.text = "위도:${currentLocation?.latitude} 경도:${currentLocation?.longitude}\n$now"
 
                 // Schedule the next update in 1 second (1000 milliseconds)
                 handler.postDelayed(this, 1000)
@@ -69,24 +96,5 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return binding.root
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
+
