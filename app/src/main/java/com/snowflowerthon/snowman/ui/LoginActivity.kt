3f9a71fcc6f37f713db1fe724df5cdefa4578250
Seoa-S <0325sandy@ewhain.net>
2024-01-12 10:21:32 +0900

    xml: ì로그인 화면 xml 파일 구성

--- app/src/main/java/com/snowflowerthon/snowman/ui/LoginActivity.kt
+++ app/src/main/java/com/snowflowerthon/snowman/ui/LoginActivity.kt
@@ -6,17 +6,17 @@ import com.snowflowerthon.snowman.R
 import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
+import com.snowflowerthon.snowman.databinding.ActivityLoginBinding
+import com.snowflowerthon.snowman.databinding.ActivityLoginBinding.inflate
 
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        KakaoSdk.init(this, "69a3cfd1a816a40b18dc20cd4b8a300d")
         setContentView(R.layout.activity_login)
-
-
-
-
+        KakaoSdk.init(this, "69a3cfd1a816a40b18dc20cd4b8a300d")
 
 
     }
